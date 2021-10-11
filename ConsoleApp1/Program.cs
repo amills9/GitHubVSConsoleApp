553b26f572cf556b371780b28bef6f4e71c04587
amills9 <89893713+amills9@users.noreply.github.com>
2021-10-11 13:18:36 +1100

    Next Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declare Variable and type
+            double myNum1;
+            double myNum2;
+            double myAns;
+            string myOps;
+
+            // Ask for first number
+            Console.Write("Enter an integer value: ");
+            myNum1 = Double.Parse(Console.ReadLine());
+
+            // Ask for operation
+            Console.Write("Enter an operator [ Subtract - , Plus + , Multiply * or Division / ]: ");
+            myOps = Console.ReadLine();
+
+            // Ask for second number
+            Console.Write("Enter another integer value: ");
+            myNum2 = Double.Parse(Console.ReadLine());
+
+
+            // Check what operation has been entered
+            switch (myOps)
+            {
+
+                case ("-"):
+                    myAns = myNum1 - myNum2;
+                    Console.WriteLine(myNum1 + " minus " + myNum2 + " equals " + myAns);
+                    break;
+                case ("+"):
+                    myAns = myNum1 + myNum2;
+                    Console.WriteLine(myNum1 + " plus " + myNum2 + " equals " + myAns);
+                    break;
+                case ("*"):
+                    myAns = myNum1 * myNum2;
+                    Console.WriteLine(myNum1 + " times " + myNum2 + " equals " + myAns);
+                    break;
+                case ("/"):
+                    myAns = myNum1 / myNum2;
+                    Console.WriteLine(myNum1 + " divided " + myNum2 + " equals " + myAns);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
